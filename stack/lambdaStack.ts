27cc55d6c4bb33281798f435606893296000cb82
ytakada <yuki.takada@ytakada.dev>
2022-11-05 16:48:47 +0000

    Add infra stack.

---
+++ stack/lambdaStack.ts
@@ -0,0 +1,47 @@
+import { LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway';
+import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
+import { Construct, Duration, Stack, StackProps } from '@aws-cdk/core';
+
+export class LambdaStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const savePostFunction = new Function(
+            this,
+            'savePostFunction',
+            {
+                functionName: 'savePostFunction',
+                runtime: Runtime.PROVIDED_AL2,
+                handler: 'handler',
+                code: Code.fromAsset(`${__dirname}/../target/debug/cdk/save-post`),
+                timeout: Duration.seconds(10)
+            }
+        )
+
+        // const deletePostFunction = new Function(
+        //     this,
+        //     'deletePostFunction',
+        //     {
+        //         functionName: 'deletePostFunction',
+        //         runtime: Runtime.PROVIDED_AL2,
+        //         handler: 'handler',
+        //         code: Code.fromAsset(`${__dirname}/../../functions/delete-post/target/cdk/debug`),
+        //         timeout: Duration.seconds(10)
+        //     }
+        // )
+
+        // Define rest api.
+        const api = new RestApi(
+            this,
+            'restApi',
+            {
+                restApiName: "ytakada.dev API"
+            }
+        );
+        // Define resource.
+        const posts = api.root.addResource('posts');
+        const singlePost = posts.addResource('{id}');
+        singlePost.addMethod("PUT", new LambdaIntegration(savePostFunction));
+        // singlePost.addMethod('DELETE', new LambdaIntegration(deletePostFunction))
+    }
+}
---
+++ stack/stack.ts
@@ -0,0 +1,13 @@
+import { App } from '@aws-cdk/core';
+import { LambdaStack } from './lambdaStack';
+
+export default class Stack {
+    public lambdaStack: LambdaStack;
+
+    constructor(app: App) {
+        this.lambdaStack = new LambdaStack(app, 'ytakada-dev-stack', {})
+    }
+}
+
+const app = new App;
+new Stack(app);
